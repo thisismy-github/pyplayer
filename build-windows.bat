cd7237d7d254eeb50eff2e1de8e88086d4018c0b
thisismy-github <48284263+thisismy-github@users.noreply.github.com>
2022-03-26 00:11:38 -0400

    updater.exe is now a standalone executable

--- build-windows.bat
+++ build-windows.bat
@@ -7,7 +7,7 @@ pyinstaller executable/updater.spec --noconfirm --distpath executable/compiled -
 copy .\executable\include .\executable\compiled\release
 copy .\bin\ffmpeg.exe .\executable\compiled\release\ffmpeg.exe
 copy .\executable\compiled\launcher\pyplayer.exe .\executable\compiled\release\pyplayer.exe
-copy .\executable\compiled\updater\updater.exe .\executable\compiled\release\updater.exe
+copy .\executable\compiled\updater.exe .\executable\compiled\release\updater.exe
 
 @RD /S /Q ".\executable\compiled\launcher"
-@RD /S /Q ".\executable\compiled\updater"
+del ".\executable\compiled\updater.exe"
