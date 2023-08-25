be28350c392c2a9a38516d299d07ac2eb80f7dda
thisismy-github <48284263+thisismy-github@users.noreply.github.com>
2023-08-25 03:45:27 -0400

    Compiling improvements, updated `requirements.txt`
    This rewords `executable/include/readme.txt`, reorders and adds
    comments to `executable/exclude.txt`, adds stricter version limits to
    `requirements.txt` while updating existing packages (within reason),
    and improves `executable/build.py` by evaluating excluded files earlier,
    checking for existing config files (first in the `executable` directory,
    then in the prior build directory) and copying them into the new build
    if detected, and reorganizing the folders so that the actual build is
    no longer contained within a nested subfolder.

--- executable/build.py
+++ executable/build.py
@@ -15,8 +15,10 @@ pjoin = os.path.join
 PLATFORM = platform.system()
 CWD = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.dirname(CWD)
-RELEASE_DIR = pjoin(CWD, 'compiled', 'release')
+RELEASE_DIR = pjoin(CWD, 'compiling', 'release')
+FINAL_RELEASE_DIR = pjoin(CWD, 'compiled')
 PYQT_DIR = pjoin(RELEASE_DIR, 'PyQt5')
+EXPECTED_SUBDIRS = ('plugins', 'pyqt5', 'themes')
 
 
 # ensure python is running from a venv, if possible
@@ -36,9 +38,36 @@ if 'venv' not in sys.executable.split(os.sep):
 def compile():
     print(f'\nCompiling PyPlayer (sys.executable="{sys.executable}")...\n')
     pyinstaller = f'"{sys.executable}" -m PyInstaller'
-    args = f'--distpath "{pjoin(CWD, "compiled")}" --workpath "{pjoin(CWD, "build")}"'
+    args = f'--distpath "{pjoin(CWD, "compiling")}" --workpath "{pjoin(CWD, "build")}"'
     subprocess.call(f'{pyinstaller} "{pjoin(CWD, "main.spec")}" --noconfirm {args}')
     subprocess.call(f'{pyinstaller} "{pjoin(CWD, "updater.spec")}" --noconfirm {args}')
+
+    print('Checking for pre-existing config files to reuse...')
+    override_config_path = pjoin(CWD, 'config.ini')
+    existing_config_path = pjoin(FINAL_RELEASE_DIR, 'config.ini')
+    if os.path.exists(override_config_path):
+        print('Copying config-override from current directory...')
+        shutil.copyfile(override_config_path, pjoin(RELEASE_DIR, 'config.ini'))
+    elif os.path.exists(existing_config_path):
+        print('Copying existing config from previous build...')
+        shutil.copyfile(existing_config_path, pjoin(RELEASE_DIR, 'config.ini'))
+    else: print('No pre-existing config files detected, skipping...')
+
+    if os.path.exists(FINAL_RELEASE_DIR):
+        print('Deleting old build...')
+        shutil.rmtree(FINAL_RELEASE_DIR)
+    else: print('No previous build detected, skipping...')
+
+    print(f'Deleting files defined in {pjoin(CWD, "exclude.txt")}...')
+    with open(pjoin(CWD, 'exclude.txt')) as exclude:
+        for line in exclude:
+            line = line.strip()
+            if line and line[0] not in '#;' and line[:2] != '//':
+                for path in glob.glob(pjoin(RELEASE_DIR, line)):
+                    print(f'exists={os.path.exists(path)} - {path}')
+                    if os.path.exists(path):
+                        if os.path.isdir(path): shutil.rmtree(path)
+                        else: os.remove(path)
 
     print('Copying cacert.pem...')
     certifi_dir = pjoin(RELEASE_DIR, 'certifi')
@@ -53,7 +82,7 @@ def compile():
 
     print('Moving updater to PyQt folder...')
     name = 'updater' + ('.exe' if PLATFORM == 'Windows' else '')
-    shutil.move(pjoin(CWD, 'compiled', name), pjoin(PYQT_DIR, name))
+    shutil.move(pjoin(os.path.dirname(RELEASE_DIR), name), pjoin(PYQT_DIR, name))
 
     print(f'Deleting non-{PLATFORM} files from plugins folder...')
     right_suffix = '-windows' if PLATFORM == 'Windows' else '-unix'
@@ -66,15 +95,6 @@ def compile():
             if os.path.isdir(path): shutil.rmtree(path)
             else: os.remove(path)
 
-    print(f'Deleting files defined in {pjoin(CWD, "exclude.txt")}...')
-    with open(pjoin(CWD, 'exclude.txt')) as exclude:
-        for line in exclude:
-            for path in glob.glob(pjoin(RELEASE_DIR, line.strip())):
-                print(f'exists={os.path.exists(path)} - {path}')
-                if os.path.exists(path):
-                    if os.path.isdir(path): shutil.rmtree(path)
-                    else: os.remove(path)
-
 
 # ---------------------
 # Windows
@@ -83,14 +103,23 @@ def compile_windows():
     compile()
     print(f'\nPerforming post-compilation tasks for {PLATFORM}...')
 
+    def move(folder):
+        for pattern in ('*.pyd', '*.dll'):
+            for path in glob.glob(pjoin(folder, pattern)):
+                print(f'{path} -> {pjoin(PYQT_DIR, os.path.basename(path))}')
+                shutil.move(path, pjoin(PYQT_DIR, os.path.basename(path)))
+
     print('Moving .pyd and .dll files to PyQt folder...')
-    for pattern in ('*.pyd', '*.dll'):
-        for path in glob.glob(pjoin(RELEASE_DIR, pattern)):
-            print(f'{path} -> {pjoin(PYQT_DIR, os.path.basename(path))}')
-            shutil.move(path, pjoin(PYQT_DIR, os.path.basename(path)))
+    move(RELEASE_DIR)
+    for filename in os.listdir(RELEASE_DIR):
+        if filename.lower() in EXPECTED_SUBDIRS: continue
+        folder = pjoin(RELEASE_DIR, filename)
+        if not os.path.isdir(folder): continue
+        move(folder)
+        shutil.rmtree(folder)
 
     print('Moving python3*.dll back to root folder...')
-    for path in glob.glob(pjoin(PYQT_DIR, 'python*.dll')):
+    for path in glob.glob(pjoin(PYQT_DIR, 'python3*.dll')):
         filename = os.path.basename(path)
         print(path, filename, filename != 'python3.dll')
         if filename != 'python3.dll':
@@ -105,6 +134,10 @@ if __name__ == '__main__':
     while True:
         try:
             compile_windows() if PLATFORM == 'Windows' else compile()
+
+            print('Compile finished, renaming "compiling" to "compiled"...')
+            os.renames(RELEASE_DIR, FINAL_RELEASE_DIR)
+
             choice = input('\nDone! Type anything to exit, or press enter to recompile... ')
             if choice != '': break
         except:
--- executable/include/readme.txt
+++ executable/include/readme.txt
@@ -1,9 +1,10 @@
-This folder is for FFmpeg as well as required VLC files.
+This folder is for FFmpeg/FFprobe and required libVLC files.
 
-If you already have FFmpeg or VLC installed on your computer
-(assuming they're up-to-date), PyPlayer will be able to access
-these files automatically. If so, you are free to delete their
-associated files from this folder if you wish.
+If you already have FFmpeg, FFprobe, or VLC installed on your
+computer (assuming they're up-to-date), PyPlayer will be able
+to access these files automatically. If so, you are free to
+delete their associated files from this folder if you wish.
 
-For developers: If you implement a new feature but get unexpected
-results, try using a full plugins folder from a VLC installation.
+For contributors: If you implement a new feature but get
+unexpected results, try using a full plugins folder from
+a VLC installation.
